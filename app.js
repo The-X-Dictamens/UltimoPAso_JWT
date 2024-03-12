35d6abea1dc4234aee3c3e83b4bc857888f15930
Alexxomby <72172506+Alexxomby@users.noreply.github.com>
2024-03-12 02:53:23 -0600

    Ya a mimir

--- app.js
+++ app.js
@@ -5,12 +5,30 @@ const dotenv = require('dotenv')
 const cookieParser = require('cookie-parser')
 
 const app = express()
-
+//seteado de motoro de poatnillas
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.render('gelouda ')
-})
+//ahora seteo de carptea public
+app.use(express.static('public'))
+
+//pa que procede datos
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+//seteamos las variables de entrono
+
+dotenv.config({ path: './env/.env' })
+
+//setep de ;as galletitas
+app.use(cookieParser)
+
+
+//llamar al router
+app.use('/', require('./routes/router'))
+
+//app.get('/', (req, res) => {
+    //res.send('gelouda ')
+//}) nos manejaremos desde el router
 app.listen(3000, () => {
     console.log('Hola niger')
 })
