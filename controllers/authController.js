d07e038c8d3da4f2ae3ddfb7ea37659f0cb65c7a
Alexxomby <72172506+Alexxomby@users.noreply.github.com>
2024-03-15 03:40:31 -0600

    Guarda Perfectamente
    Guarda perfectamentee a la perfeccion en la base de datos

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,15 +6,31 @@ const conexion = require('../database/db')
 
 const { promisify } = require('util')
 const { use } = require('../routes/router')
+const { error } = require('console')
 
 //PRocedimiento parea hacer la registracion
 
 exports.register = async (req, res) => {
     //con esto llamaremos a los muchachines
 
-    const name = req.body.name
+
+    try {
+        const name = req.body.name
     const user = req.body.user
     const pass = req.body.pass
     //pero esto tenemos que especificarlo en nueestro enrutador
-    console.log(name + ' - ' + user +' - '+ ' - '+ pass)
+    let passHast = await bcrypt.hash(pass, 8)
+
+        conexion.query('INSERT INTO Users SET ?', { user: user, name: name, pass: passHast }, (error, results) => {
+            if (error) { console.log(error) }
+            res.redirect('/')
+        })
+    } catch (error) {
+        console.log(error)
+
+    }
+
+    
+
+    //ahora creamos la sentencia sql para insertar los datos
 }
