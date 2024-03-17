d40a821a0e30df49b149251ed464fc1047eaeeb5
Alexxomby <72172506+Alexxomby@users.noreply.github.com>
2024-03-16 23:13:15 -0600

    Revisar que es lo que cambia para hacerlo bien

--- app.js
+++ app.js
@@ -1,39 +1,36 @@
 const express = require('express')
-
 const dotenv = require('dotenv')
-
 const cookieParser = require('cookie-parser')
 
 const app = express()
-//seteado de motoro de poatnillas
-app.set('view engine', 'ejs');
+
+//seteamos el motor de plantillas
+app.set('view engine', 'ejs')
 
 //seteamos la carpeta public para archivos estáticos
+app.use(express.static('public'))
 
-app.use(express.static('public'));
-
-//pa que procede datos
-app.use(express.urlencoded({ extended: true }))
+//para procesar datos enviados desde forms
+app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
-//seteamos las variables de entrono
+//seteamos las variables de entorno
+dotenv.config({path: './env/.env'})
 
-dotenv.config({ path: './env/.env' })
-
-//setep de ;as galletitas
-//app.use(cookieParser)
-
+//para poder trabajar con las cookies
+app.use(cookieParser())
 
 //llamar al router
 app.use('/', require('./routes/router'))
 
-//app.get('/', (req, res) => {
-    //res.send('gelouda ')
-//}) nos manejaremos desde el router
+//Para eliminar la cache 
+app.use(function(req, res, next) {
+    if (!req.user)
+        res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+    next();
+});
 
 
-app.listen(3000, () => {
-    console.log('Hola niriano')
-    console.log('server running en http://localhost:3000' )
-
+app.listen(3000, ()=>{
+    console.log('SERVER UP runnung in http://localhost:3000')
 })
--- controllers/authController.js
+++ controllers/authController.js
@@ -26,7 +26,7 @@ exports.register = async (req, res) => {
             res.redirect('/')
         })
     } catch (error) {
-        console.log(error)
+        console.log('primer catch'+error)
 
     }
 
@@ -37,68 +37,61 @@ exports.register = async (req, res) => {
 
 //cierren los ojos que se viene el inicio de sesion
 
-exports.login = async (req, res) => {
+exports.login = async (req, res)=>{
     try {
         const user = req.body.user
-        const pass = req.body.passw
+        const pass = req.body.pass        
 
-
-        if (!user || !pass) {
-            res.render('login', {
-                alert: true,
+        if(!user || !pass ){
+            res.render('login',{
+                alert:true,
                 alertTitle: "Advertencia",
-                alertMessage: "Ingresa algo o se te metera ",
-                alertIcon: 'info',
+                alertMessage: "Ingrese un usuario y password",
+                alertIcon:'info',
                 showConfirmButton: true,
                 timer: false,
                 ruta: 'login'
             })
-        } else {
-            conexion.query('SELECT * FROM users WHERE  user = ?', [user], async (error, results) => {
-                
-                if (results.length == 0 || ! ( await bcrypt.compare(pass, results[0].pass))) {
+        }else{
+            conexion.query('SELECT * FROM users WHERE user = ?', [user], async (error, results)=>{
+                if( results.length == 0 || ! (await bcrypt.compare(pass, results[0].pass)) ){
                     res.render('login', {
                         alert: true,
                         alertTitle: "Error",
-                        alertMessage: "ACuerdate de tu pass o se metera ",
-                        alertIcon: 'error',
+                        alertMessage: "Usuario y/o Password incorrectas",
+                        alertIcon:'error',
                         showConfirmButton: true,
                         timer: false,
-                        ruta: 'login'
+                        ruta: 'login'    
                     })
-                    
-                } else {//si esta ttodo bien ya esta pos validado
-                    //hijo de su pinche madre, no cerraste la boca y ya sae te vino el jason       WT
+                }else{
+                    //inicio de sesión OK
                     const id = results[0].id
-                    //ocupamos nuestra clave secreta para el cifradito
-                    const token = jwt.sign({ id: id }, process.env.JWT_SECRETO, {
+                    const token = jwt.sign({id:id}, process.env.JWT_SECRETO, {
                         expiresIn: process.env.JWT_TIEMPO_EXPIRA
                     })
-                    
-                    console.log("CODIGOS NUCLES " + token + " para  a " + user)
-                    
-                    //ahora configuremos nuestra galletita de mota
+                    //generamos el token SIN fecha de expiracion
+                   //const token = jwt.sign({id: id}, process.env.JWT_SECRETO)
+                   console.log("TOKEN: "+token+" para el USUARIO : "+user)
 
-                    const cookieOptions = {
-                        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
+                   const cookiesOptions = {
+                        expires: new Date(Date.now()+process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
                         httpOnly: true
-                    }
-
-                    res.cookie('jwt', token, cookieOptions)
-                    res.render('login', {
+                   }
+                   res.cookie('jwt', token, cookiesOptions)
+                   res.render('login', {
                         alert: true,
-                        alertTitle: " Nectado",
-                        alertMessage: "Ya no se metera ",
-                        alertIcon: 'aucces',
+                        alertTitle: "Conexión exitosa",
+                        alertMessage: "¡LOGIN CORRECTO!",
+                        alertIcon:'success',
                         showConfirmButton: false,
                         timer: 800,
                         ruta: ''
-                    })
+                   })
                 }
-                
             })
         }
     } catch (error) {
-        console.log(error)
+        console.log('ultiomo catch'+error)
     }
 }
