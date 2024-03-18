5d604f664e2d2c0f5e7e9bf6f9ac448380f749ec
Alexxomby <72172506+Alexxomby@users.noreply.github.com>
2024-03-18 16:26:43 -0600

    a llroar

--- routes/router.js
+++ routes/router.js
@@ -1,44 +1,23 @@
-//las rutas para el controlador
-
 const express = require('express')
+const router = express.Router()
 
-const router = express.Router() 
-
-const conexion = require('../database/db')
-
-
-
-//1.R
 const authController = require('../controllers/authController')
 
-//esto e spa las vistar
-router.get('/', authController.isAuthenticated , (req, res) => {
-    //conexion()
-    res.render('index' ,{user:req.user})
+//router para las vistas
+router.get('/', authController.isAuthenticated, (req, res)=>{    
+    res.render('index', {user:req.user})
 })
-
-
-
-router.get('/login', (req, res) => {
+router.get('/login', (req, res)=>{
     res.render('login', {alert:false})
 })
-
-
-router.get('/register', (req, res) => {
-    res.render('register')//pero primero necesitamos hacer un require de nuestro controlador 1.R
+router.get('/register', (req, res)=>{
+    res.render('register')
 })
 
-//aqui pa las vistas
 
-//rputer para los metodos del controler
-//hay que fijarnos en el formulario que metodo pusimos
+//router para los métodos del controller
 router.post('/register', authController.register)
-
 router.post('/login', authController.login)
-//verbo hhtp get
-
 router.get('/logout', authController.logout)
-
-
 
 module.exports = router
